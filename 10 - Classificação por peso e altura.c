57e759b2edfebf67be06f436cafaf97a6f2eb773
Lucas Farias <83430368+Fariaslr@users.noreply.github.com>
2022-12-03 11:48:43 -0300

    :hammer:

--- 10 - Classificação por peso e altura.c
+++ 10 - Classificação por peso e altura.c
@@ -2,42 +2,32 @@
 #include <locale.h>
 
 /*
-	9) FaÁa um programa que leia a altura e o peso de uma pessoa. De acordo com a 
-	tabela a seguir, verifique e mostre qual a classificaÁ„o dessa pessoa.
+	10) Fa√ßa um programa que leia tr√™s n√∫meros inteiros positivos e efetue o c√°lculo de
+	uma das seguintes m√©dias de acordo com um valor num√©rico digitado pelo usu√°rio e mostrado na tabela a seguir:
 */
 
 int main(){
-	float peso, altura;
-	setlocale(LC_ALL,"");
+	
+    int x,y,z;
+    float mediaGeometrica, mediaPonderada, mediaHarmonica, mediaAritemtica;
+    setlocale(LC_ALL,"");
     
-    printf("Peso: ");
-    scanf("%f",&peso);
-    
-    printf("Altura: ");
-    scanf("%f",&altura);
-    
-    if(peso <= 60){
-    	
-    	printf("ClassificaÁ„o ");
-    	if(altura <= 1.20) printf("A");
-		if(altura > 1.20 && altura <= 1.70) printf("B");
-		if(altura > 1.70) printf("C");	
-			
-	}else if(peso > 60 && peso <= 90){
-		
-		printf("ClassificaÁ„o ");
-		if(altura <= 1.20) printf("D");
-		if(altura > 1.20 && altura <= 1.70) printf("E");
-		if(altura > 1.70) printf("F");
-		
-	}else{
-		
-		printf("ClassificaÁ„o ");
-		if(altura <= 1.20) printf("G");
-		if(altura > 1.20 && altura <= 1.70) printf("H");
-		if(altura > 1.70) printf("I");
-		
-	}
+    printf("Digite o valor de x: ");
+    scanf("%d",&x);
+    printf("Digite o valor de y: ");
+    scanf("%d",&y);
+    printf("Digite o valor de z: ");
+    scanf("%d",&z);
+
+    mediaGeometrica = x*y*z;
+    mediaPonderada = (x+2*y+3*z)/6;
+    mediaHarmonica = 1/((1/x)+(1/y)+(1/z));
+    mediaAritemtica = (x+y+z)/3;
+
+    printf("\nM√©dia geom√©trica: %.2f\n",mediaGeometrica);
+    printf("M√©dia ponderada: %.2f\n",mediaPonderada);
+    printf("M√©dia harm√¥nica: %.2f\n",mediaHarmonica);
+    printf("M√©dia aritm√©tica: %.2f\n",mediaAritemtica);
     
    return 0;
 }
